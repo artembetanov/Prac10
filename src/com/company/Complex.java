0168e71a19f454e5e5b6cd665d42dea9ff9295bb
Артем Бетанов <artembetanov@MacBook-Pro-Artem-2.local>
2020-12-15 16:36:41 +0300

    Initial commit

---
+++ src/com/company/Complex.java
@@ -0,0 +1,38 @@
+
+package com.company;
+
+public class Complex  {
+
+    int real;
+    int image;
+    public Complex(){
+        this.real = 0;
+        this.image = 0;
+    }
+
+    public Complex(int real,int image)
+    {
+        this.real= real;
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+    public int getReal() {
+        return real;
+    }
+    public void setImage(int image) {
+        this.image = image;
+    }
+    public void setReal(int real) {
+        this.real = real;
+    }
+    @Override
+    public String toString() {
+        return "Complex{" +
+                "real=" + real +
+                ", image=" + image +
+                '}';
+    }
+}
